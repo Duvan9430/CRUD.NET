01a88076c7206270f46f1a2e6ab601db4c063fc2
Duvan9430 <45524875+Duvan9430@users.noreply.github.com>
2022-04-07 19:44:19 -0500

    Add files via upload

---
+++ Compania/Form1.Designer.vb
@@ -0,0 +1,211 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.btnBuscar = New System.Windows.Forms.Button()
+        Me.btnEliminar = New System.Windows.Forms.Button()
+        Me.btnGuardar = New System.Windows.Forms.Button()
+        Me.btnNuevo = New System.Windows.Forms.Button()
+        Me.txtBuscar = New System.Windows.Forms.TextBox()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.txtTELEFONO = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.txtNOMBRE = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.txtID = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Panel1.SuspendLayout()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Panel1
+        '
+        Me.Panel1.BackColor = System.Drawing.SystemColors.ControlDark
+        Me.Panel1.Controls.Add(Me.Label1)
+        Me.Panel1.Dock = System.Windows.Forms.DockStyle.Top
+        Me.Panel1.Location = New System.Drawing.Point(0, 0)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(800, 81)
+        Me.Panel1.TabIndex = 13
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(33, 25)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(254, 25)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Registro de empleados"
+        '
+        'btnBuscar
+        '
+        Me.btnBuscar.Location = New System.Drawing.Point(337, 250)
+        Me.btnBuscar.Name = "btnBuscar"
+        Me.btnBuscar.Size = New System.Drawing.Size(113, 23)
+        Me.btnBuscar.TabIndex = 25
+        Me.btnBuscar.Text = "Buscar"
+        Me.btnBuscar.UseVisualStyleBackColor = True
+        '
+        'btnEliminar
+        '
+        Me.btnEliminar.Location = New System.Drawing.Point(337, 188)
+        Me.btnEliminar.Name = "btnEliminar"
+        Me.btnEliminar.Size = New System.Drawing.Size(75, 23)
+        Me.btnEliminar.TabIndex = 24
+        Me.btnEliminar.Text = "Eliminar"
+        Me.btnEliminar.UseVisualStyleBackColor = True
+        '
+        'btnGuardar
+        '
+        Me.btnGuardar.Location = New System.Drawing.Point(337, 157)
+        Me.btnGuardar.Name = "btnGuardar"
+        Me.btnGuardar.Size = New System.Drawing.Size(75, 23)
+        Me.btnGuardar.TabIndex = 23
+        Me.btnGuardar.Text = "Guardar"
+        Me.btnGuardar.UseVisualStyleBackColor = True
+        '
+        'btnNuevo
+        '
+        Me.btnNuevo.Location = New System.Drawing.Point(337, 128)
+        Me.btnNuevo.Name = "btnNuevo"
+        Me.btnNuevo.Size = New System.Drawing.Size(75, 23)
+        Me.btnNuevo.TabIndex = 22
+        Me.btnNuevo.Text = "Nuevo"
+        Me.btnNuevo.UseVisualStyleBackColor = True
+        '
+        'txtBuscar
+        '
+        Me.txtBuscar.Location = New System.Drawing.Point(15, 253)
+        Me.txtBuscar.Name = "txtBuscar"
+        Me.txtBuscar.Size = New System.Drawing.Size(287, 20)
+        Me.txtBuscar.TabIndex = 21
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.AllowUserToAddRows = False
+        Me.DataGridView1.AllowUserToDeleteRows = False
+        Me.DataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill
+        Me.DataGridView1.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Location = New System.Drawing.Point(0, 279)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.ReadOnly = True
+        Me.DataGridView1.Size = New System.Drawing.Size(800, 150)
+        Me.DataGridView1.TabIndex = 20
+        '
+        'txtTELEFONO
+        '
+        Me.txtTELEFONO.Location = New System.Drawing.Point(89, 188)
+        Me.txtTELEFONO.Name = "txtTELEFONO"
+        Me.txtTELEFONO.Size = New System.Drawing.Size(221, 20)
+        Me.txtTELEFONO.TabIndex = 19
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(12, 191)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(64, 13)
+        Me.Label4.TabIndex = 18
+        Me.Label4.Text = "TELEFONO"
+        '
+        'txtNOMBRE
+        '
+        Me.txtNOMBRE.Location = New System.Drawing.Point(89, 162)
+        Me.txtNOMBRE.Name = "txtNOMBRE"
+        Me.txtNOMBRE.Size = New System.Drawing.Size(221, 20)
+        Me.txtNOMBRE.TabIndex = 17
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(12, 165)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(54, 13)
+        Me.Label3.TabIndex = 16
+        Me.Label3.Text = "NOMBRE"
+        '
+        'txtID
+        '
+        Me.txtID.Location = New System.Drawing.Point(89, 136)
+        Me.txtID.Name = "txtID"
+        Me.txtID.ReadOnly = True
+        Me.txtID.Size = New System.Drawing.Size(100, 20)
+        Me.txtID.TabIndex = 15
+        Me.txtID.Text = "0"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(12, 139)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(18, 13)
+        Me.Label2.TabIndex = 14
+        Me.Label2.Text = "ID"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 432)
+        Me.Controls.Add(Me.Panel1)
+        Me.Controls.Add(Me.btnBuscar)
+        Me.Controls.Add(Me.btnEliminar)
+        Me.Controls.Add(Me.btnGuardar)
+        Me.Controls.Add(Me.btnNuevo)
+        Me.Controls.Add(Me.txtBuscar)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Controls.Add(Me.txtTELEFONO)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.txtNOMBRE)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.txtID)
+        Me.Controls.Add(Me.Label2)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents Label1 As Label
+    Friend WithEvents btnBuscar As Button
+    Friend WithEvents btnEliminar As Button
+    Friend WithEvents btnGuardar As Button
+    Friend WithEvents btnNuevo As Button
+    Friend WithEvents txtBuscar As TextBox
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents txtTELEFONO As TextBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents txtNOMBRE As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents txtID As TextBox
+    Friend WithEvents Label2 As Label
+End Class
---
+++ Compania/Form1.vb
@@ -0,0 +1,53 @@
+﻿Public Class Form1
+    Dim Operaciones As New ServicioDatos.empleadosSoapClient
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub btnNuevo_Click(sender As Object, e As EventArgs) Handles btnNuevo.Click
+        Limpiar()
+    End Sub
+
+    Private Sub Limpiar()
+        txtID.Text = "0"
+        txtNOMBRE.Clear()
+        txtTELEFONO.Clear()
+
+        txtNOMBRE.Focus()
+    End Sub
+
+    Private Sub btnGuardar_Click(sender As Object, e As EventArgs) Handles btnGuardar.Click
+        If txtID.Text = "0" Then
+            Operaciones.NuevoRegistro(txtNOMBRE.Text, txtTELEFONO.Text)
+            DataGridView1.DataSource = Operaciones.BuscaRegistro(txtBuscar.Text).Tables(0)
+        Else
+            Operaciones.ActualizarRegistro(txtID.Text, txtNOMBRE.Text, txtTELEFONO.Text)
+            DataGridView1.DataSource = Operaciones.BuscaRegistro(txtBuscar.Text).Tables(0)
+        End If
+
+    End Sub
+
+    Private Sub btnEliminar_Click(sender As Object, e As EventArgs) Handles btnEliminar.Click
+        Operaciones.EliminaRegistro(txtID.Text)
+        DataGridView1.DataSource = Operaciones.BuscaRegistro(txtBuscar.Text).Tables(0)
+    End Sub
+
+    Private Sub btnBuscar_Click(sender As Object, e As EventArgs) Handles btnBuscar.Click
+        DataGridView1.DataSource = Operaciones.BuscaRegistro(txtBuscar.Text).Tables(0)
+    End Sub
+
+    Private Sub DataGridView1_CellContentClick(sender As Object, e As DataGridViewCellEventArgs) Handles DataGridView1.CellContentClick
+
+    End Sub
+
+    Private Sub DataGridView1_CellEnter(sender As Object, e As DataGridViewCellEventArgs) Handles DataGridView1.CellEnter
+        txtID.Text = DataGridView1.CurrentRow.Cells("ID").Value
+        txtNOMBRE.Text = DataGridView1.CurrentRow.Cells("NOMBRE").Value.ToString
+        txtTELEFONO.Text = DataGridView1.CurrentRow.Cells("TELEFONO").Value.ToString
+    End Sub
+
+    Private Sub txtID_TextChanged(sender As Object, e As EventArgs) Handles txtID.TextChanged
+
+    End Sub
+End Class
